4a216983a373cca739f1311f8950d93dbc627816
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2024-06-26 11:54:09 +0530

    home page responsiveness june 26

--- src/ui/Partners.jsx
+++ src/ui/Partners.jsx
@@ -4,40 +4,62 @@ import styled from "styled-components";
 const Container = styled.div`
   background-color: #fdfdfd;
   width: 100%;
-  height: min-content;
+  height: max-content;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  align-items: center;
-  padding: 5rem 15rem;
-  padding-bottom: 10rem;
+  align-items: stretch;
+  padding: 3vmax 10vmax;
+  /* padding-bottom: 10rem; */
+  /* background-color: yellow; */
+  /* border: 0.5rem solid red; */
+  @media (max-width: 768px) {
+    padding: 2vmax;
+  }
 `;
 
 const Heading = styled.h2`
   text-transform: uppercase;
   font-weight: 500;
-  font-size: 4rem;
-  margin-bottom: 5rem;
+  font-size: 3.5vmax;
+  margin-bottom: 3vmax;
   color: black;
   letter-spacing: 4px;
+  /* border: 2px solid red; */
+  text-align: center;
 `;
 
 const LogoContainer = styled.div`
   display: flex;
+  /* border: 2px solid red; */
+  justify-content: space-around;
+  /* flex-wrap: wrap; */
+  @media (max-width: 768px) {
+    flex-wrap: nowrap;
+    overflow-x: auto;
+  }
+  .scrollable::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const Logo = styled.img`
   width: 13rem;
   height: 13rem;
+  margin: 1vmax;
   border-radius: 50%;
   object-fit: cover;
   transition: transform 0.3s ease-in-out;
-  margin-right: 5rem;
   cursor: pointer;
   position: relative;
 
   &:hover {
     transform: scale(1.1);
+  }
+  @media (max-width: 768px) {
+    height: 9rem;
+    width: 9rem;
+    aspect-ratio: 1/1;
   }
 `;
 
