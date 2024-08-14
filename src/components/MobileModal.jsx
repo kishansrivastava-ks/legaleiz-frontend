515d2a79e1dc9050c53638d0824995543a7ce79a
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2024-08-14 22:15:48 +0530

    changes in slideout navigation bar modal

--- src/components/MobileModal.jsx
+++ src/components/MobileModal.jsx
@@ -5,8 +5,10 @@ import styled from "styled-components";
 const ModalBackground = styled.div`
   position: absolute;
   top: 0;
-  left: -9.5rem;
-  width: 101vw;
+  /* left: -9.5rem; */
+  /* left: 0; */
+  right: 0;
+  width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.6);
   display: flex;
